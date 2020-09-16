c3fca2990017e5e4c71fd5d45b4db73f7afb6876
Arnon Nunes <arnonguimaraes@gmail.com>
2020-09-16 17:13:09 -0300

    lisca de tarefas com localstorage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useMemo, useCallback} from 'react';
+import './app.css';
 
 function App() {
+
+     const [tarefas, setTarefas] = useState([]);
+     const [inpute, setInpute] = useState('');
+
+
+     useEffect(()=>{
+          const tarefasStorage = localStorage.getItem('tarefas');
+
+          if(tarefasStorage){
+               setTarefas(JSON.parse(tarefasStorage))
+          }
+     }, [])
+
+     useEffect(() => {
+          localStorage.setItem('tarefas', JSON.stringify(tarefas));
+     }, [tarefas])
+
+     const handleAdd = useCallback(() =>{
+          if(inpute === ''){
+               alert('Adicione alguma tarefa!');
+               return false;
+          }
+          
+          let data = {id: Date.now(), tarefa: inpute}
+          setTarefas([...tarefas, data]);
+          setInpute('');
+     },[inpute, tarefas]);
+
+     const totalTarefas = useMemo(()=> tarefas.length, [tarefas])
+
+     const totalConcluido = useMemo(()=> {
+          let concluidos = tarefas.filter(repo => repo.concluid);
+          return concluidos.length
+     },[tarefas])
+     
+     const total = useMemo(()=> {
+          let totalTarefa = totalTarefas; 
+          totalTarefa -= totalConcluido;
+         return totalTarefa;
+     }, [tarefas])
+
+
+     function concluido (id){
+          const newConcluido = tarefas.map(repo => {
+               return repo.id === id ? {...repo, concluid: !repo.concluid} : repo
+          })
+          setTarefas(newConcluido);
+     }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+         <h1>Lista de Tarefas</h1>
+         <div className="conteiner-add">
+               <input type="text" value={inpute} autoFocus onChange={(e) => setInpute(e.target.value)}/>
+               <button type="button" onClick={handleAdd}>Adicionar</button>
+         </div>
+         <div className="info">
+              <strong>Tarefas: {totalTarefas}!</strong>
+               <strong>Concluidos: {totalConcluido}!</strong>
+               <strong>Total não Concluida {total}</strong>
+         </div>
+         <div id="list">
+               <ul>
+                    {tarefas.map(repo => (
+                         <div key={repo.id}>
+                              <li>
+                                   {repo.tarefa}
+                                   {repo.concluid && <span>Concluido</span>}
+                              <button type="button" onClick={()=>concluido(repo.id)}>Marcar como Concluida</button>
+                              </li>
+                         </div>
+                    ))}
+               </ul>
+         </div>
     </div>
   );
 }
